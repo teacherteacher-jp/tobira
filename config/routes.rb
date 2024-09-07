64ecb28364b121fbfb5018a919c4efb0499d0419
juneboku <juneboku@gmail.com>
2024-09-07 23:08:36 +0900

    招待するときに指定するRoleとしないRoleを選べるように

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get    "/invitations",           to: "invitations#index"
   get    "/roles",                 to: "roles#index"
   post   "/roles/sync",            to: "roles/sync#create"
+  post   "/roles/:role_id/usable", to: "roles/usable#create", as: :role_usable
+  delete "/roles/:role_id/usable", to: "roles/usable#destroy"
 
   root "root#index"
 end
