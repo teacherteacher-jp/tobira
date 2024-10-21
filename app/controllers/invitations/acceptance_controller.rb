69d03dd7bee9283c87c0d2fe1452c939b603fea8
juneboku <juneboku@gmail.com>
2024-10-21 20:45:39 +0900

    存在しない招待コードと使用済み招待コードでメッセージを出し分ける

--- app/controllers/invitations/acceptance_controller.rb
+++ app/controllers/invitations/acceptance_controller.rb
@@ -1,12 +1,15 @@
 class Invitations::AcceptanceController < ApplicationController
   def create
-    invitation = Invitation.not_used.find_by(code: params[:code].to_s.strip)
+    code = params[:code].to_s.strip
+    invitation = Invitation.find_by(code:)
 
-    if invitation
+    if invitation.nil?
+      redirect_to(root_path, notice: "招待コード「#{code}」は無効です。管理者に問い合わせてください。")
+    elsif invitation.used?
+      redirect_to(root_path, notice: "招待コード「#{code}」は使用済みです。Discordサーバーに参加できているかご確認ください。")
+    else
       invitation.used_by!(current_member)
       redirect_to(root_path, notice: "Discordサーバに招待しました！")
-    else
-      redirect_to(root_path, notice: "招待コードが無効のようです。管理者に問い合わせてください。")
     end
   end
 end
