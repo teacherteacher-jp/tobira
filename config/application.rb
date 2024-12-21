49d7550ff29e41dd0b77994555d0ab39b3adafad
juneboku <juneboku@gmail.com>
2024-12-21 23:22:33 +0900

    rails app:updateによる変更を、いったんぜんぶ入れる

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,5 @@ module Tobira
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    config.time_zone = "Asia/Tokyo"
   end
 end
